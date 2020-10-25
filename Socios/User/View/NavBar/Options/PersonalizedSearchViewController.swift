f5ada7c45aa6bb8b6424e088ea74678f0ec41bcf
caposcar1998 <42816786+caposcar1998@users.noreply.github.com>
2020-10-24 22:07:23 -0500

    ML se pone modelo

--- Socios/User/View/NavBar/Options/PersonalizedSearchViewController.swift
+++ Socios/User/View/NavBar/Options/PersonalizedSearchViewController.swift
@@ -7,6 +7,11 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+//ML solo funciona a partir de iOS 11
+@available(iOS 11.0, *)
+
 
 class PersonalizedSearchViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -20,6 +25,40 @@ class PersonalizedSearchViewController: UIViewController, UIImagePickerControlle
         // Do any additional setup after loading the view.
         miPicker.delegate = self
         image.image = UIImage(named: "ImagePlaceholder")
+    }
+    
+
+    @IBAction func MlBusqueda() {
+        
+        //instanciar el modelo de la red neuronal
+        let modelFile = ML_Socios_1()
+        let model = try! VNCoreMLModel(for: modelFile.model)
+        //Convertir la imagen obtenida a CIImage
+        let imagenCI = CIImage(image: image.image!)
+        //Crear un controlador para el manejo de la imagen, este es un requerimiento para ejecutar la solicitud del modelo
+        let handler = VNImageRequestHandler(ciImage: imagenCI!)
+        //Crear una solicitud al modelo para el análisis de la imagen
+        let request = VNCoreMLRequest(model: model, completionHandler: resultadosModelo)
+        try! handler.perform([request])
+    }
+    
+
+    
+    func resultadosModelo(request: VNRequest, error: Error?)
+    {
+        guard let results = request.results as? [VNClassificationObservation] else { fatalError("No hubo respuesta del modelo ML")}
+        var bestPrediction = ""
+        var bestConfidence: VNConfidence = 0
+        //recorrer todas las respuestas en búsqueda del mejor resultado
+        for classification in results{
+            if (classification.confidence > bestConfidence){
+                bestConfidence = classification.confidence
+                bestPrediction = classification.identifier
+            }
+        }
+        let resultado = bestPrediction+" "+String(bestConfidence)
+        print(resultado)
+        
     }
     
     @IBAction func selectPicture(_ sender: Any) {
