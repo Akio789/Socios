f1ef06f14a6cfda5bb2ea5c80711cc52cba44133
Manue Ortiz Hernández <manuoh09@gmail.com>
2020-11-02 19:55:54 -0600

    Total now is displayed

---
+++ Socios/User/View/Cart/CartViewController.swift
@@ -0,0 +1,60 @@
+//
+//  CartViewController.swift
+//  Socios
+//
+//  Created by Manuel Ortiz on 02/11/20.
+//  Copyright © 2020 A01651395. All rights reserved.
+//
+
+import UIKit
+import FirebaseFirestore
+import Social
+import Firebase
+
+class CartViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    var totalPriceOfCart: Double!
+    let db = Firestore.firestore()
+    var user = Auth.auth().currentUser
+    let storage = Storage.storage()
+    var storageRef: StorageReference = StorageReference()
+    var orderRef: StorageReference = StorageReference()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getTotalPrice()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func getTotalPrice(){
+        db.collection("carts").whereField("user", isEqualTo: user!.uid).getDocuments() {
+            (querySnapshot, err)
+            in guard let querySnapshot = querySnapshot else {
+                print("error")
+                return
+            }
+            if !querySnapshot.isEmpty && querySnapshot.documents.count > 0 {
+                //Add to user Cart
+                let cartInfo = querySnapshot.documents.first?.data()
+                let totalPrice = cartInfo?["total"] as! Double
+                self.totalLabel.text = "\(totalPrice)"
+            }else {
+                print("Error")
+                return
+            }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
