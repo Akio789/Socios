31c99f989648ba1d18282cf3f6e0d8fea71e4d38
Manue Ortiz Hernández <manuoh09@gmail.com>
2020-11-02 23:24:07 -0600

    Total price of Cart is displayed

--- Socios/User/View/Cart/ContainerCartViewController.swift
+++ Socios/User/View/Cart/ContainerCartViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 import LocalAuthentication
+import FirebaseFirestore
+import Social
+import Firebase
 
 class ContainerCartViewController: UIViewController {
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    let db = Firestore.firestore()
+    var user = Auth.auth().currentUser
+    let storage = Storage.storage()
+    var storageRef: StorageReference = StorageReference()
+    var orderRef: StorageReference = StorageReference()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        getTotalPrice()
         // Do any additional setup after loading the view.
+    }
+    
+    func getTotalPrice(){
+        db.collection("carts").whereField("user", isEqualTo: user!.uid).getDocuments() {
+            (querySnapshot, err)
+            in guard let querySnapshot = querySnapshot else {
+                print("error")
+                return
+            }
+            if !querySnapshot.isEmpty && querySnapshot.documents.count > 0 {
+                //Add to user Cart
+                let cartInfo = querySnapshot.documents.first?.data()
+                let totalPrice = cartInfo?["total"] as! Double
+                self.totalLabel.text = "\(totalPrice)"
+                return
+            }else {
+                print("Error")
+                return
+            }
+        }
+        return
     }
     
     /*
