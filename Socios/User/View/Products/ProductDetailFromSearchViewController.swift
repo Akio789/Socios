82308758d9140303a265ed630c2e4df11cba5d1b
Manue Ortiz Hernández <manuoh09@gmail.com>
2020-11-19 01:39:43 -0600

    Retrieve information from an id of a store

--- Socios/User/View/Products/ProductDetailFromSearchViewController.swift
+++ Socios/User/View/Products/ProductDetailFromSearchViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 import FirebaseFirestore
 import Social
 import Firebase
+import WatchConnectivity
 
-class ProductDetailFromSearchViewController: UIViewController {
+class ProductDetailFromSearchViewController: UIViewController, WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        //code
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        //code
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        //code
+    }
+    
 
     
     @IBOutlet weak var addToCart: UIButton!
@@ -21,7 +34,7 @@ class ProductDetailFromSearchViewController: UIViewController {
     @IBOutlet weak var productName: UILabel!
     @IBOutlet weak var productImage: UIImageView!
     @IBOutlet weak var prodcuRating: UILabel!
-    
+    var miSesion: WCSession! = nil
     let db = Firestore.firestore()
     var user = Auth.auth().currentUser
     let storage = Storage.storage()
@@ -39,7 +52,9 @@ class ProductDetailFromSearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        miSesion = WCSession.default
+        miSesion.delegate = self
+        miSesion.activate()
         self.orderRef = self.storageRef.child(productimageUrl)
         self.orderRef.getData(maxSize: 1 * 1024 * 1024) { data, error in
           if let error = error {
@@ -59,8 +74,54 @@ class ProductDetailFromSearchViewController: UIViewController {
     
     
     @IBAction func enviarReloj(_ sender: Any) {
+        var latitud: Double!
+        var longitud: Double!
+        var storeId: String!
+//        var point: GeoPoint!
+        db.collection("productos").whereField("id", isEqualTo: productId).getDocuments() {
+            (querySnapshot, err)
+            in guard let querySnapshot = querySnapshot else {
+                print("error")
+                return
+            }
+            if !querySnapshot.isEmpty && querySnapshot.documents.count > 0 {
+                //Add to user Cart
+                let productInfo = querySnapshot.documents.first?.data()
+                storeId = productInfo?["storeId"] as? String
+                self.db.collection("tiendas").whereField("id", isEqualTo: storeId).getDocuments() {
+                    (querySnapshot, err)
+                    in guard let querySnapshot = querySnapshot else {
+                        print("Error")
+                        return
+                    }
+                    if !querySnapshot.isEmpty && querySnapshot.documents.count > 0 {
+                        let storeInfo = querySnapshot.documents.first?.data()
+                        let coords = storeInfo?["localization"] as! GeoPoint
+                        latitud = coords.latitude
+                        longitud = coords.longitude
+                        let mensaje = ["Latitud":latitud,"Longitud":longitud]
+                        print(mensaje)
+                        self.miSesion.sendMessage(mensaje, replyHandler: nil, errorHandler: self.manejoError(err:))
+                    }else{
+                        print("Error retrieving information from store.")
+                        return
+                    }
+                }
+            }else {
+                print("Error retrieving information from product.")
+                return
+            }
+        }
+        
+        
+        
+  
+        
     }
     
+    func manejoError( err: Error)->Void{
+        print(err.localizedDescription)
+    }
 
     @IBAction func shareSocial(_ sender: Any) {
         let message = "Encuentra \(self.productNameA), solamente en \(self.productSellerEntry)."
